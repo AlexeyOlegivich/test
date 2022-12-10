ca20e7d7949982ecb3de4b1827b98051b5a494f4
AlexeyOlegivich <ifns6193@gmail.com>
2022-12-10 13:27:56 +0800

    final

--- 1week.py
+++ 1week.py
@@ -5,6 +5,5 @@ city = ['London', 'SVO', 'череповец?n&lang=ru&M&m&T']
 for i in range(3):
     response = requests.get(url+city[i])
     response.raise_for_status()
-    #print(response.text)
-    print(response.url)
+    print(response.text)
     i += 1
