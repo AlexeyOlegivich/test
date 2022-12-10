4c091ae65e34c2be230b796f03c4054e683ae614
AlexeyOlegivich <ifns6193@gmail.com>
2022-12-10 09:51:11 +0800

    1st

--- 1week.py
+++ 1week.py
@@ -1,10 +1,10 @@
 import requests
 
 url = 'https://wttr.in/'
-city = ['London', 'SVO', 'череповец']
+city = ['London', 'SVO', 'череповец?n&lang=ru&M&m&T']
 for i in range(3):
     response = requests.get(url+city[i])
     response.raise_for_status()
-    print(response.text)
-    #print(response.url)
+    #print(response.text)
+    print(response.url)
     i += 1
